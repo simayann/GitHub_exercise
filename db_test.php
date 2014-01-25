54de15149cf295c674d8ba6e91a14feb751e9d37
simayann <simayann@castle69.com>
2014-01-25 10:26:22 +0900

    Modified:mysql_connect() function -> mysqli_connect()

--- db_test.php
+++ db_test.php
@@ -1,8 +1,18 @@
 <?php
+	/* mysql_connect()古い関数 非推奨
 	$link = mysql_connect('localhost','root','');
+
 	if(!$link){
 			die('接続失敗！<br>'.mysql_error());
 	}else{
 		echo '接続成功';
+	}
+	*/
+
+	$link = mysqli_connect('localhost','root','');
+	if(!$link){
+		die('接続失敗<br>'.mysqli_connect_error());
+	}else{
+		echo '接続成功！';
 	}
 ？>
